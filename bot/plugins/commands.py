f0e9e2994befa7bad46633db6f84548aedfef0b3
aryastark-jr <82750569+aryastark-jr@users.noreply.github.com>
2021-07-24 17:57:04 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/ECManager_Bot'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/ECManager_Bot')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/ECManager_Bot')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
